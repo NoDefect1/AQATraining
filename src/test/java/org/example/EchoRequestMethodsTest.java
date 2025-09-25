588e4dd1980865648d67a23e1c056057c0fd7fd4
NoDefect1 <Chempalov13@gmail.com>
2025-09-26 00:42:01 +0300

    1

--- src/test/java/org/example/EchoRequestMethodsTest.java
+++ src/test/java/org/example/EchoRequestMethodsTest.java
@@ -1,11 +1,12 @@
 package org.example;
 
-import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 
 public class EchoRequestMethodsTest {
 
